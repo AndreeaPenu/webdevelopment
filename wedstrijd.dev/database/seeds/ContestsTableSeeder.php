ab430cfea6134bf9ea29e6c875a3c6bc50ec4c82
andreea penu <andreea_penu@hotmail.com>
2017-11-07 17:18:40 +0100

    generate contests

---
+++ wedstrijd.dev/database/seeds/ContestsTableSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+use Carbon\Carbon;
+use Illuminate\Database\Seeder;
+
+class ContestsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('contests')->delete();
+
+        DB::table('contests')->insert(array(
+
+            array(
+                'start'=>Carbon::now(),
+                'end'=>Carbon::now()->addDays(7)),
+            array(
+                'start'=>Carbon::now()->addDays(14),
+                'end'=>Carbon::now()->addDays(21)),
+            array(
+                'start'=>Carbon::now()->addDays(28),
+                'end'=>Carbon::now()->addDays(35)),
+            array(
+                'start'=>Carbon::now()->addDays(42),
+                'end'=>Carbon::now()->addDays(49))
+        ));
+    }
+}
